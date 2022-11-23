98fd7408dfe039019f71d94b71c531441c8818f9
Mama <adndrijsavickij@gmail.com>
2022-11-23 19:31:28 +0200

    first commit

--- src/main/java/com/example/tictactoe/controller/MainController.java
+++ src/main/java/com/example/tictactoe/controller/MainController.java
@@ -2,13 +2,18 @@ package com.example.tictactoe.controller;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import org.kordamp.bootstrapfx.scene.layout.Panel;
 
@@ -34,12 +39,20 @@ public class MainController  {
     public void initialize() {
         for(int i = 0; i < 10;i++) // ?????????????!!?!?!?!?!??!?? d(-_-)b <--- you  // line more black =)
             Platform.runLater(this::drawGF);
-    }
 
+        GameField.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                GameField.getChildren().add(new Circle(event.getX(),event.getY(),5,Color.RED));
+            }
+        });
+
+    }
     @FXML
     public void onclk(ActionEvent actionEvent) {
         drawGF();
     }
+
     private void drawGF(){
         var width = GameField.getWidth();
         var height = GameField.getHeight();
