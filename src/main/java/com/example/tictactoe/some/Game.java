4bc575e06ee476f3f74813a05ecd78804f478319
Mama <adndrijsavickij@gmail.com>
2022-11-30 02:13:13 +0200

    v0.2

--- src/main/java/com/example/tictactoe/some/Game.java
+++ src/main/java/com/example/tictactoe/some/Game.java
@@ -10,11 +10,11 @@ import javafx.scene.paint.Paint;
 import javafx.scene.shape.Line;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.Arrays;
+
 
 public class Game {
     private final Pane field;
-    private ArrayList<Title> titles = null;
+    private final ArrayList<Title> titles = new ArrayList<>(9);
     private static boolean move = false;
     public Game(Pane field){
         this.field = field;
@@ -22,7 +22,6 @@ public class Game {
     public void newGame(){
         field.getChildren().clear();// bad
         drawGF();
-        titles = new ArrayList<>(9);
 
         double width = field.getWidth()/3;
         double height = field.getHeight()/3;
@@ -43,44 +42,43 @@ public class Game {
     }
     
     private void newMove(Title title){
-        if(move) title.setFigura(Krestik.class);
+        if(move) title.setFigura(Krestik.class); // bad
         else title.setFigura(Nolik.class);
         field.getChildren().add(title.getFigura().getGroup());
         move = !move;
+        whoWin(title);
     }
 
     public void onClick(Double X, Double Y) {
         for (var i : titles) {
             if (i.isEmpty() && i.isIn(X, Y)) {
-                newMove(i);
-                whoWin(titles.indexOf(i));
-                break;
+                newMove(i); break;
             }
         }
     }
 
-    private void whoWin(int id){
+    private void whoWin(Title title){
         if(!titles.get(0).isEmpty() || !titles.get(4).isEmpty() || !titles.get(4).isEmpty()) {
-            var v = titles.get(id);
-            final int MX = 3, MY = 3, LINE = 3;
+            int id = titles.indexOf(title);
 
-            if (v.equals(titles.get(hor_ver(id,1,0))) && v.equals(titles.get(hor_ver(id,2,0)))) {
-                winLine(id,0);// --
+            if (title.equals(titles.get(hor_ver(id,1,0))) && title.equals(titles.get(hor_ver(id,2,0)))) {
+                winLine(title,0);// --
             }
-            else if (v.equals(titles.get(hor_ver(id,0,1))) && v.equals(titles.get(hor_ver(id,0,2)))) {
-                winLine(id,1);// |
+            else if (title.equals(titles.get(hor_ver(id,0,1))) && title.equals(titles.get(hor_ver(id,0,2)))) {
+                winLine(title,1);// |
             }
             else if ((id==0 || id == 4 || id== 8) && (titles.get(0).equals(titles.get(4)) && titles.get(0).equals(titles.get(8)))) {
-                winLine(id,2);// \
+                winLine(title,2);// \
             }
             else if ((id==2 || id == 4 || id== 6) && (titles.get(2).equals(titles.get(4)) && titles.get(2).equals(titles.get(6)))) {
-                winLine(id,3);// /
-                // FIX 29.11.2022
+                winLine(title,3);// /
             } else if (titles.stream().noneMatch(Title::isEmpty)) {
                 Platform.runLater(()-> field.setOnMouseClicked(event -> newGame()));
             }
         }
     }
+
+    @Deprecated
     private int hor_ver(int id, int X, int Y){
         int xmax = 3, ymax = 3;
         int x = id % 3, y = id / 3;
@@ -88,7 +86,7 @@ public class Game {
         y += y + Y >= ymax ? (Y - ymax) : Y;
         return x + y * 3;
     }
-
+    @Deprecated
     private int rdig_ldig(int id, int X, int Y){
         int xmax = 3, ymax = 3;
         int x = id % 3, y = id / 3;
@@ -97,19 +95,23 @@ public class Game {
         return x + y * 3;
     }
 
-    private void winLine(int id, int ll){
-        var v = titles.get(id);
+    /**
+     *
+     * @param ll - 0 -> --, 1 -> |, 2 -> \, 3 -> /.
+     */
+    private void winLine(Title title, int ll){
         Line line;
         int zero = 10;
         switch (ll){
             // --
-            case 0-> line = new Line(zero,v.getCenterY(),field.getWidth(),v.getCenterY());
+            case 0-> line = new Line(zero,title.getCenterY(),field.getWidth(),title.getCenterY());
             // |
-            case 1-> line = new Line(v.getCenterX(),zero,v.getCenterX(),field.getHeight());
+            case 1-> line = new Line(title.getCenterX(),zero,title.getCenterX(),field.getHeight());
             // \
             case 2-> line = new Line(zero,zero,field.getWidth(),field.getHeight());
             // /
             case 3-> line = new Line(field.getWidth(),zero,zero,field.getHeight());
+            // error
             default -> line = new Line(100,100,200,200);
         }
         line.setStrokeWidth(6);
@@ -119,19 +121,20 @@ public class Game {
 
     }
 
-
-
     private void drawGF(){
         var width = field.getWidth();
         var height = field.getHeight();
+
         Line hl1 = new Line(width / 3, 0, width / 3, height);
         Line hl2 = new Line((width / 3) * 2, 0, (width / 3) * 2, height);
         Line vl1 = new Line(0, height / 3, width, height / 3);
         Line vl2 = new Line(0, (height / 3) * 2, width, (height / 3) * 2);
+
         hl1.setStrokeWidth(5);
         hl2.setStrokeWidth(5);
         vl1.setStrokeWidth(5);
         vl2.setStrokeWidth(5);
+
         field.getChildren().add(new Group(hl1, hl2, vl1, vl2));
     }
 
