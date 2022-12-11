94407407d7bc1fc369fa4fbf63b680364c036218
ADMIN <andrijsavitskij86@gmail.com>
2022-12-11 15:09:41 +0200

    v.0.3

--- src/main/java/com/example/tictactoe/HelloApplication.java
+++ src/main/java/com/example/tictactoe/HelloApplication.java
@@ -11,9 +11,11 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mainHBox-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(),700,500);//1280,720);
+        Scene scene = new Scene(fxmlLoader.load(),800,500);//1280,720);
         //stage.setResizable(false); // no resize
         stage.setTitle("Tic-Tac-Toe");
+        stage.setMinHeight(400);
+        stage.setMinWidth(600);
         stage.setScene(scene);
         stage.show();
 
